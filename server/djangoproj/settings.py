cab750fa1b64f607ab77d7ae4022890afc387530
wxb1 <wendellbuckner@sbcglobal.net>
2025-05-26 16:12:10 -0500

    line fix attempt - Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -97,7 +97,7 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.' \
+        'django.contrib.auth.password_validation.'
         'UserAttributeSimilarityValidator',
     },
     {
