0de723c17ab676eea22343939f85bbd0e117c371
wxb1 <wendellbuckner@sbcglobal.net>
2025-05-26 16:14:12 -0500

    final line continuation - Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -29,8 +29,10 @@ SECRET_KEY =\
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost',
-                 'https://wendellbuckn-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
-CSRF_TRUSTED_ORIGINS = ['https://wendellbuckn-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+                 'https://wendellbuckn-8000.'
+                 'theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+CSRF_TRUSTED_ORIGINS = ['https://wendellbuckn-8000.'
+                        'theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
