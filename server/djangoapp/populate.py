940169a3a961f62161bb6f096b1b157b04d85be5
wxb1 <wendellbuckner@sbcglobal.net>
2025-05-26 15:46:21 -0500

    fix lint attempt- Update populate.py

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -33,6 +33,8 @@ def initiate():
       # Add more CarModel instances as needed
     ]
     for data in car_model_data:
-        CarModel.objects.create(name=data['name'], \
-        car_make=data['car_make'], type=data['type'], \
-        year=data['year'])
+        CarModel.objects.create(name=data['name'], 
+                                car_make=data['car_make'], 
+                                type=data['type'], 
+                                year=data['year'])
+        
