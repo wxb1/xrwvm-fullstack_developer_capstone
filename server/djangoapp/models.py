93300083c7bea896e025698878a9f74c2c44ba33
wxb1 <wendellbuckner@sbcglobal.net>
2025-05-26 09:55:56 -0500

    initial fixing linit - Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,7 +1,7 @@
 # Uncomment the following imports before adding the Model code
 
 from django.db import models
-from django.utils.timezone import now
+#from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
@@ -31,6 +31,7 @@ class CarMake(models.Model):
 
     def __str__(self):
         return self.name  # Return the name as the string representation
+
 
 class CarModel(models.Model):
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
