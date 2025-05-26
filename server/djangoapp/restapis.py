e8284f3671ab2bac411b5b89db9e8d082e74777e
wxb1 <wendellbuckner@sbcglobal.net>
2025-05-26 16:26:54 -0500

    fix lint problems - Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -18,12 +18,13 @@ sentiment_analyzer_url = os.getenv(
 # request_url = sentiment_analyzer_url+"analyze/"+text
 # Add code for retrieving sentiments
 
+
 # def post_review(data_dict):
 # Add code for posting review
 def get_request(endpoint, **kwargs):
     params = ""
-    if(kwargs):
-        for key,value in kwargs.items():
+    if(kwargs): 
+        for key, value in kwargs.items():
             params=params+key+"="+value+"&"
     request_url = backend_url+endpoint+"?"+params
     print("GET from {} ".format(request_url))
@@ -35,20 +36,22 @@ def get_request(endpoint, **kwargs):
         # If any error occurs
         print("Network exception occurred")
 
+
 def analyze_review_sentiments(text):
     request_url = sentiment_analyzer_url+"analyze/"+text
     try:
-            # Call get method of requests library with URL and parameters
+        # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
     except Exception as err:
         print(f"Unexpected {err=}, {type(err)=}")
         print("Network exception occurred")
 
+
 def post_review(data_dict):
     request_url = backend_url+"/insert_review"
     try:
-        response = requests.post(request_url,json=data_dict)
+        response = requests.post(request_url, json=data_dict)
         print(response.json())
         return response.json()
     except:
