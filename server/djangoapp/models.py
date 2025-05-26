4621861bb3c88c5b6154ae2e92c912258d0b2adc
wxb1 <wendellbuckner@sbcglobal.net>
2025-05-26 10:01:08 -0500

    fixing linting - ident - Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -48,7 +48,7 @@ class CarModel(models.Model):
         validators=[
             MaxValueValidator(2023),
             MinValueValidator(2015)
-    ])
+        ])
     # Other fields as needed
 
     def __str__(self):
